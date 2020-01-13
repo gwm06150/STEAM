23de8145cdff7d5df036c1c215bfd34c85fafa71
Gene <gwm06150@vtc.vsc.edu>
2020-01-13 18:25:20 -0500

    single channel PWM test

--- tests/test-rgb-display/test-rgb-display.ino
+++ tests/test-rgb-display/test-rgb-display.ino
@@ -1,9 +1,15 @@
+#define RED_CHANNEL 3
+#define GREEN_CHANNEL 5
+#define BLUE_CHANNEL 6
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(RED_CHANNEL, OUTPUT);
+  pinMode(GREEN_CHANNEL, OUTPUT);
+  pinMode(BLUE_CHANNEL, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  analogWrite(RED_CHANNEL, 20);
 }
