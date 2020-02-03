94e316320f1812b9ddf776e5ea6a5d46c1d6885e
Maniaci <gwm06150@vtc.vsc.edu>
2020-02-03 15:34:13 -0500

    basic cycling

--- tests/test-solenoids/test-solenoids.ino
+++ tests/test-solenoids/test-solenoids.ino
@@ -1,9 +1,21 @@
+
+#define SOL_A 7
+#define SOL_B 8
+
 void setup()
 {
-  
+  pinMode(SOL_A, OUTPUT);
+  pinMode(SOL_B, OUTPUT);
 }
 
 void loop() 
 {
-  
+  digitalWrite(SOL_A, HIGH);
+  delay(500);
+  digitalWrite(SOL_A, LOW);
+  delay(100);
+  digitalWrite(SOL_B, HIGH);
+  delay(500);
+  digitalWrite(SOL_B, LOW);
+  delay(100);
 }
