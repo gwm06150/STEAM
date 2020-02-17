434471ddc35cb0d1d7af2680ce3c2cad21d08e2f
McGuinness <jjm09141@vtc.vsc.edu>
2020-02-17 17:04:25 -0500

    added stepper code

--- tests/test-flow-control-subsys/test-flow-control-subsys.ino
+++ tests/test-flow-control-subsys/test-flow-control-subsys.ino
@@ -1,9 +1,32 @@
-void setup() {
-    // do some stuff
 
+#define PIN_DIR  4
+#define PIN_STEP 7
+
+
+void setup() {
+  pinMode(PIN_DIR, OUTPUT);
+  pinMode(PIN_STEP, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(PIN_DIR, HIGH);
 
-
+  for(int i = 0; i < 1200; i++) {
+    digitalWrite(PIN_STEP, HIGH);
+    delay(10);
+    digitalWrite(PIN_STEP, LOW);
+    delay(10);
+  }
+  delay(1000);
+  
+  digitalWrite(PIN_DIR, LOW);
+  
+  for(int i = 0; i < 1200; i++) {
+    digitalWrite(PIN_STEP, HIGH);
+    delay(10);
+    digitalWrite(PIN_STEP, LOW);
+    delay(10);
+  }
+  delay(1000);
+  
 }
