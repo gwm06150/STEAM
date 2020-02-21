c58d79b283938488b1cb48bfe9fe4f28e82aa79d
McGuinness <jjm09141@vtc.vsc.edu>
2020-02-21 15:54:19 -0500

    modify test code

--- tests/test-flow-control-subsys/test-flow-control-subsys.ino
+++ tests/test-flow-control-subsys/test-flow-control-subsys.ino
@@ -1,31 +1,45 @@
+// Gene and John
+// February 12, 2020
+// Test Code for the flow controller subsystem
 
+// Defined Pins
 #define PIN_DIR  4
 #define PIN_STEP 7
+#define SOL_1 8 // first solenoid
+#define SOL_2 9 // second solenoid
+
+// Global variables
+int counter = 0;
+
 
 
 void setup() {
   pinMode(PIN_DIR, OUTPUT);
   pinMode(PIN_STEP, OUTPUT);
+  pinMode(SOL_1, OUTPUT);
+  pinMode(SOL_2, OUTPUT);
 }
+
+
 
 void loop() {
   digitalWrite(PIN_DIR, HIGH);
 
-  for(int i = 0; i < 1200; i++) {
+  for(int i = 0; i < 1600; i++) {
     digitalWrite(PIN_STEP, HIGH);
-    delay(10);
+    delay(1);
     digitalWrite(PIN_STEP, LOW);
-    delay(10);
+    delay(1);
   }
   delay(1000);
   
   digitalWrite(PIN_DIR, LOW);
   
-  for(int i = 0; i < 1200; i++) {
+  for(int i = 0; i < 1600; i++) {
     digitalWrite(PIN_STEP, HIGH);
-    delay(10);
+    delay(1);
     digitalWrite(PIN_STEP, LOW);
-    delay(10);
+    delay(1);
   }
   delay(1000);
   
