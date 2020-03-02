a4a207172bb7bfe4533a96aa589528359afab950
McGuinness <jjm09141@vtc.vsc.edu>
2020-03-02 15:05:00 -0500

    wrote the step flow valve functions

--- tests/test-flow-control-subsys/test-flow-control-subsys.ino
+++ tests/test-flow-control-subsys/test-flow-control-subsys.ino
@@ -6,7 +6,7 @@
 #include stdlib.h
 
 // Define Pins
-#define PIN_DIR 4
+#define PIN_DIR 4 // set pin high to close valve
 #define PIN_STEP 7
 #define SOL_1 8 // first solenoid
 #define SOL_2 9 // second solenoid
@@ -16,13 +16,17 @@
 #define LISTEN 1
 #define ERROR 2
 
+// Define Values
+#define pulseWidth 1 // set in milliseconds
+
 // Global variables
 int engineState = SELF_TEST;
 int period = 500;
-int valveOnTimeStart1 = 0;
-int minimumStepTimeFC = 5; // time in milliseconds
+int pulseTimer = 0;
+int valveStepCount = 0;
+int pulseState = 0;
 
-unsigned long time_now = 0; // this should take several days before running over
+unsigned long timeNow = 0; // this should take several days before running over
 
 
 
@@ -38,75 +42,121 @@ void setup() {
 void loop() {
   // Read input from serial and set valves as needed
 
+  // Update Time at start of Loop
+  timeNow = millis();
+
   // START OF SELF TEST STATE__________________________________________________________________________________________________________
-if (engineState == SELF_TEST){
-  // Set UP and Test Engine
-  // If self test fails move to error State
+  if (engineState == SELF_TEST){
+    // Set UP and Test Engine
+    // If self test fails move to error State
 
-  // If self test passes move to error state
+    // If self test passes move to error state
 
-  // FOR DEMONSTRATION
-  if (serial.Read('1')){
-    engineState = LISTEN;
-    // if user prompts self test pass, move to listen
-  }
-  if (serial.Read('2')){
-    engineState = ERROR;
-    // if user prompts error, move to error state
-  }
-} // END OF SELF TEST STATE
+    // FOR DEMONSTRATION
+    if (serial.Read('1')){
+      engineState = LISTEN;
+      // if user prompts self test pass, move to listen
+    }
+    if (serial.Read('2')){
+      engineState = ERROR;
+      // if user prompts error, move to error state
+    }
+  } // END OF SELF TEST STATE
 
-// START OF ERROR STATE__________________________________________________________________________________________________________
-if (engineState == ERROR){
-  // Wait until the controller prompts to move back to self test
-  // If controller prompts, move to self test
+  // START OF ERROR STATE__________________________________________________________________________________________________________
+  if (engineState == ERROR){
+    // Wait until the controller prompts to move back to self test
+    // If controller prompts, move to self test
 
-  // else, just wait
+    // else, just wait
 
-  // FOR DEMONSTRATION
-  if (serial.Read('3')){
-    engineState = SELF_TEST;
-    // if the user provides all clear return to self test
-  }
-} // END OF ERROR STATE
+    // FOR DEMONSTRATION
+    if (serial.Read('3')){
+      engineState = SELF_TEST;
+      // if the user provides all clear return to self test
+    }
+  } // END OF ERROR STATE
 
-//START OF LISTEN STATE__________________________________________________________________________________________________________
-if (engineState == LISTEN){
-  // Update Position of flow controller
-  // get input angle, convert to steps, call steps until matched
+  //START OF LISTEN STATE__________________________________________________________________________________________________________
+  if (engineState == LISTEN){
+    // Update Position of flow controller
+    // get input angle, convert to steps, call steps until matched
 
-  // Update cycle speed of valves
+    // Update cycle speed of valves
 
-} // END OF LISTEN STATE
+  } // END OF LISTEN STATE
 } // end of loop
 
 void stepFlowValveClosed(){
   // used to step the flow contorller closed
-  // millis used to control the width of the pulse
+  // millis() used to control the width of the pulse
   // a min pulse width for the controller must be maintained in order for it to step properly
 
-  // set the direction pin
+  if(pulseState = 0){
+    // set the direction pin
+    digitalWrite(PIN_DIR, HIGH); // CLOSE VALVE
+    
+    // raise the pusle pin high
+    digitalWrite(PIN_STEP, HIGH);
+    
+    // log the start time of the pulse
+    pulseTimer = millis();
 
-  // raise the pusle pin high
+    // update the pulse state
+    pulseState = 1;
+  }
 
-  // log the start time of the pulse
+  if(pulseState = 1){
+    // check that the pulse has started
+    if((millis() - pulseTimer) > pulseWidth){
+      // check that the pulse has gone on long enough
+      
+      // bring the pusle pin low
+      digitalWrite(PIN_STEP, LOW);
 
-  // once the pulse has gone on long enough lower the pulse pin and update the step count
-}
+      // Decrement the step count
+      valveStepCount--;
+      pulseState = 0;
+
+    }
+  }
+} // END OF FLOW VALVE CLOSE
+
 void stepFlowValveOpen(){
-  // used to step the flow contorller open
-  // millis used to control the width of the pulse
+  // used to step the flow contorller closed
+  // millis() used to control the width of the pulse
   // a min pulse width for the controller must be maintained in order for it to step properly
-  
-  // set the direction pin
 
-  // raise the pusle pin high
+  if(pulseState = 0){
+    // set the direction pin
+    digitalWrite(PIN_DIR, LOW); // OPEN VALVE
+    
+    // raise the pusle pin high
+    digitalWrite(PIN_STEP, HIGH);
+    
+    // log the start time of the pulse
+    pulseTimer = millis();
 
-  // log the start time of the pulse
+    // update the pulse state
+    pulseState = 1;
+  }
 
-  // once the pulse has gone on long enough lower the pulse pin and update the step count
+  if(pulseState = 1){
+    // check that the pulse has started
+    if((millis() - pulseTimer) > pulseWidth){
+      // check that the pulse has gone on long enough
+      
+      // bring the pusle pin low
+      digitalWrite(PIN_STEP, LOW);
 
-}
+      // Decrement the step count
+      valveStepCount++;
+      pulseState = 0;
+
+    }
+  }
+} // END OF FLOW VALVE OPEN
+
 void solenoidValveTiming(){
   // based on the angle input from the encoder determine the angle of the valve
   
@@ -117,24 +167,3 @@ void solenoidValveTiming(){
 
   // continue to loop this and update the delay between the valve swapping in the serial read area
 }
-
-
-
-  // for(int i = 0; i < 1600; i++) {
-  //   digitalWrite(PIN_STEP, HIGH);
-  //   delay(1);
-  //   digitalWrite(PIN_STEP, LOW);
-  //   delay(1);
-  // }
-  // delay(1000);
-  
-  // digitalWrite(PIN_DIR, LOW);
-  
-  // for(int i = 0; i < 1600; i++) {
-  //   digitalWrite(PIN_STEP, HIGH);
-  //   delay(1);
-  //   digitalWrite(PIN_STEP, LOW);
-  //   delay(1);
-  // }
-  // delay(1000);
-
